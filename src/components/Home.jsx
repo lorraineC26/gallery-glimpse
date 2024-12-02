abeffbae7e4114d3ebf1e6ecde38142b6b86f927
lorraineC26 <lorraine.quinyee@gmail.com>
2024-12-02 23:40:31 +0800

    create home content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import "../styles/Home.css";
+
+import landingPic from "../../public/camera-girl.jpg";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home-container">
+      <img
+        src={landingPic}
+        alt="woman using Nikon DSLR camera"
+        className="home__hero-pic"
+      />
+      <p className="home__slogan">
+        A Picture for Every Story, A Glimpse into Beauty
+      </p>
 
-export default Home
+      <section className="features-container">
+        <h2 className="features__title">What You Can Do at GalleryGlimpse:</h2>
+        <ul className="features__list">
+          <div className="features__card">Browse images</div>
+          <div className="features__card">Mark favorites</div>
+          <div className="features__card">Customize details</div>
+        </ul>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
