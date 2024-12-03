b72d3b1f3e035f0a68c661c763dfeb9860ae1f8a
lorraineC26 <lorraine.quinyee@gmail.com>
2024-12-03 21:34:55 +0800

    lift modal states to the top

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,55 @@ import Gallery from "./components/Gallery";
 import Membership from "./components/Membership";
 import Footer from "./components/Footer";
 
+import { catPhotos } from "./mock/photos";
+
 function App() {
   const [page, setPage] = useState(document.location.pathname || "/");
 
+  // Modal comments and favorite handlers
+  const [photos, setPhotos] = useState(catPhotos);
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
+  const updateComments = (newComment) => {
+    if (selectedPhoto) {
+      const updatedPhotos = photos.map((photo) => {
+        if (photo.id === selectedPhoto.id) {
+          return {
+            ...photo,
+            comments: [...photo.comments, newComment],
+          };
+        }
+
+        return photo;
+      });
+
+      setPhotos(updatedPhotos);
+      setSelectedPhoto({
+        ...selectedPhoto,
+        comments: [...selectedPhoto.comments, newComment],
+      });
+    }
+  };
+  const toggleFavorite = () => {
+    if (selectedPhoto) {
+      const updatedPhotos = photos.map((photo) => {
+        if (photo.id === selectedPhoto.id) {
+          return {
+            ...photo,
+            isFavorite: !photo.isFavorite,
+          };
+        }
+        return photo;
+      });
+
+      setPhotos(updatedPhotos);
+      setSelectedPhoto({
+        ...selectedPhoto,
+        isFavorite: !selectedPhoto.isFavorite,
+      });
+    }
+  };
+
+  // Page routing
   useEffect(() => {
     function handlePageLoad() {
       setPage(document.location.pathname);
@@ -24,11 +70,21 @@ function App() {
 
   function renderPage() {
     if (page === "/" || page === "/home") return <Home setPage={setPage} />;
-    if (page === "/gallery") return <Gallery />;
+    if (page === "/gallery")
+      return (
+        <Gallery
+          updateComments={updateComments}
+          toggleFavorite={toggleFavorite}
+          photos={photos}
+          selectedPhoto={selectedPhoto}
+          setSelectedPhoto={setSelectedPhoto}
+        />
+      );
     if (page === "/membership") return <Membership />;
     return <p>404 - Page Not Found</p>;
   }
 
+  // Theme switch handler
   const selectedTheme = localStorage.getItem("theme");
   if (selectedTheme) {
     document.querySelector("body").setAttribute("data-theme", selectedTheme);
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import "../styles/Gallery.css";
-import { catPhotos } from "../mock/photos";
 import GalleryCard from "./GalleryCard";
 import GalleryCardModal from "./GalleryCardModal";
 
-const Gallery = () => {
-  const [photos, setPhotos] = useState(catPhotos);
+const Gallery = ({
+  updateComments,
+  toggleFavorite,
+  selectedPhoto,
+  setSelectedPhoto,
+  photos,
+}) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedPhoto, setSelectedPhoto] = useState(null);
 
   const handleModalOpen = (photo) => {
     setIsModalOpen(true);
@@ -17,47 +20,6 @@ const Gallery = () => {
   const handleModalClose = () => {
     setIsModalOpen(false);
     setSelectedPhoto(null);
-  };
-
-  const updateComments = (newComment) => {
-    if (selectedPhoto) {
-      const updatedPhotos = photos.map((photo) => {
-        if (photo.id === selectedPhoto.id) {
-          return {
-            ...photo,
-            comments: [...photo.comments, newComment],
-          };
-        }
-        
-        return photo;
-      });
-
-      setPhotos(updatedPhotos);
-      setSelectedPhoto({
-        ...selectedPhoto,
-        comments: [...selectedPhoto.comments, newComment],
-      });
-    }
-  };
-
-  const toggleFavorite = () => {
-    if (selectedPhoto) {
-      const updatedPhotos = photos.map((photo) => {
-        if (photo.id === selectedPhoto.id) {
-          return {
-            ...photo,
-            isFavorite: !photo.isFavorite,
-          };
-        }
-        return photo;
-      });
-
-      setPhotos(updatedPhotos);
-      setSelectedPhoto({
-        ...selectedPhoto,
-        isFavorite: !selectedPhoto.isFavorite,
-      });
-    }
   };
 
   return (
