6c824a0d354d09c3f7501689bbcea8a449b849ba
lorraineC26 <lorraine.quinyee@gmail.com>
2024-12-03 21:20:35 +0800

    create modal styling

--- src/components/GalleryCardModal.jsx
+++ src/components/GalleryCardModal.jsx
@@ -44,48 +44,52 @@ const GalleryCardModal = ({
         <img src={closeIcon} alt="close icon" />
       </button>
 
-      <h3 className="modal__title">{title}</h3>
-      <img src={src} alt={alt} className="modal__pic" />
-      <p className="modal__date">Uploaded: {dateUploaded}</p>
-      <p className="modal__description">{description}</p>
+      <div className="modal__content">
+        <h3 className="modal__title">{title}</h3>
+        <img src={src} alt={alt} className="modal__pic" />
+        <p className="modal__date">Uploaded: {dateUploaded}</p>
+        <p className="modal__description">{description}</p>
 
-      {/* Fav Toggler */}
-      <button onClick={toggleFavorite} className="modal__fav-toggle">
-        {isFavorite ? "Remove from Favorite" : "Add to Favorite"}
-      </button>
-
-      {/* Comments Section */}
-      <div className="modal__comments">
-        <h4>Comments</h4>
-        {comments.length > 0 ? (
-          comments.map((comment, index) => (
-            <div key={index} className="modal__comment">
-              <p>{comment}</p>
-            </div>
-          ))
-        ) : (
-          <p>No comments yet. Be the first to leave a comment!</p>
-        )}
-      </div>
-
-      {/* Comment Form */}
-      <form className="modal__comment-form" onSubmit={handleCommentSubmit}>
-        <label htmlFor="comment" className="modal__label">
-          Drop some feelings:
-        </label>
-        <textarea
-          id="comment"
-          name="comment"
-          placeholder="So iconic..."
-          value={newComment}
-          onChange={(e) => setNewComment(e.target.value)}
-          className="modal__input"
-          required
-        ></textarea>
-        <button type="submit" className="modal__btn-submit">
-          Submit
+        {/* Fav Toggler */}
+        <button onClick={toggleFavorite} className="modal__fav-toggle">
+          {isFavorite ? "🤍 Remove from Fav" : "❤️ Add to Fav"}
         </button>
-      </form>
+
+        {/* Comments Section */}
+        <div className="modal__comments">
+          <h4>Comments</h4>
+          {comments.length > 0 ? (
+            comments.map((comment, index) => (
+              <div key={index} className="modal__comment">
+                <p>{comment}</p>
+              </div>
+            ))
+          ) : (
+            <p className="modal__no-comments">
+              No comments yet. Be the first to leave a comment!
+            </p>
+          )}
+        </div>
+
+        {/* Comment Form */}
+        <form className="modal__comment-form" onSubmit={handleCommentSubmit}>
+          <label htmlFor="comment" className="modal__label">
+            Drop some feelings:
+          </label>
+          <textarea
+            id="comment"
+            name="comment"
+            placeholder="So iconic..."
+            value={newComment}
+            onChange={(e) => setNewComment(e.target.value)}
+            className="modal__input"
+            required
+          ></textarea>
+          <button type="submit" className="modal__btn-submit">
+            Submit
+          </button>
+        </form>
+      </div>
     </dialog>
   );
 };
